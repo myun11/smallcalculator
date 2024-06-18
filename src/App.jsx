21371f456237d7a798766bc9fcb47b7adb8a2540
myun11 <yun.michael.ds@gmail.com>
2024-06-18 03:28:23 -0400

    Implemented math logic and failsafes for syntax

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,52 @@ import './App.css'
 
 function App() {
   const [screen, setScreen] = useState('')
-
+  const [error, setError] = useState('')
   return (
     <>
       <div>
         <h1> Simple Calculator </h1>
+        <div className = "Screen">
+        {screen}
+        </div>
+        <div> {error} </div>
         <div className = "Calculator">
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>+</button>
+          <button onClick = {() => { setScreen(str => str + '7')}}>7</button>
+          <button onClick = {() => { setScreen(str => str + '8')}}>8</button>
+          <button onClick = {() => { setScreen(str => str + '9')}}>9</button>
+          <button onClick = {() => { setScreen(str => str + '+')}}>+</button>
           
+          <button onClick = {() => { setScreen(str => str + '4')}}>4</button>
+          <button onClick = {() => { setScreen(str => str + '5')}}>5</button>
+          <button onClick = {() => { setScreen(str => str + '6')}}>6</button>
+          <button onClick = {() => { setScreen(str => str + '-')}}>-</button>
+          
+          <button onClick = {() => { setScreen(str => str + '1')}}>1</button>
+          <button onClick = {() => { setScreen(str => str + '2')}}>2</button>
+          <button onClick = {() => { setScreen(str => str + '3')}}>3</button>
+          <button onClick = {() => { setScreen(str => str + 'x')}}>x</button>
 
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
+          <button onClick = {() => { setScreen(str => str + '0')}}>0</button>
+          <button onClick = {() => { setScreen(str => str + '.')}}>.</button>
+          <button onClick = {() => { setScreen('')}}>AC</button>
+          <button onClick = {() => { setScreen(str => str + '/')}}>/</button>
           
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>x</button>
-
-          <button>0</button>
-          <button>.</button>
-          <button>AC</button>
-
-          <button>/</button>
+          <button onClick = {() => { setScreen(str => str + '(')}}>(</button>
+          <button onClick = {() => { setScreen(str => str + ')')}}>)</button>
+          <button onClick = {() => ( setScreen(str => str.slice(0, str.length - 1)))}>&#x2190;</button>
           
-          
-          <button>(</button>
-          <button>)</button>
-          <button>Ans</button>
-          
-          <button>=</button>
+          <button onClick = {() => {
+            try {
+              let newVal = eval(screen.replace('x', '*').replace('(', '*('))
+              setScreen(() => newVal)
+              setError('')
+            } catch (error) {
+              console.log(error)
+              setError('Please fix expression')
+            }
+            
+            
+          }}>=</button>
           
 
         </div>
