f5eab6b7f86addd68913e89aa6ddc17fabddf3e2
myun11 <yun.michael.ds@gmail.com>
2024-06-19 22:22:43 -0400

    fixed edge case logic for string syntax

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <>
       <div>
         <h1> Simple Calculator </h1>
+        <h2> Link to repository: link</h2>
         <div className = "Screen">
         {screen}
         </div>
@@ -16,22 +17,22 @@ function App() {
           <button onClick = {() => { setScreen(str => str + '7')}}>7</button>
           <button onClick = {() => { setScreen(str => str + '8')}}>8</button>
           <button onClick = {() => { setScreen(str => str + '9')}}>9</button>
-          <button onClick = {() => { setScreen(str => str + '+')}}>+</button>
+          <button onClick = {() => { setScreen(str => str + ' + ')}}>+</button>
           
           <button onClick = {() => { setScreen(str => str + '4')}}>4</button>
           <button onClick = {() => { setScreen(str => str + '5')}}>5</button>
           <button onClick = {() => { setScreen(str => str + '6')}}>6</button>
-          <button onClick = {() => { setScreen(str => str + '-')}}>-</button>
+          <button onClick = {() => { setScreen(str => str + ' - ')}}>-</button>
           
           <button onClick = {() => { setScreen(str => str + '1')}}>1</button>
           <button onClick = {() => { setScreen(str => str + '2')}}>2</button>
           <button onClick = {() => { setScreen(str => str + '3')}}>3</button>
-          <button onClick = {() => { setScreen(str => str + 'x')}}>x</button>
+          <button onClick = {() => { setScreen(str => str + ' x ')}}>x</button>
 
           <button onClick = {() => { setScreen(str => str + '0')}}>0</button>
           <button onClick = {() => { setScreen(str => str + '.')}}>.</button>
           <button onClick = {() => { setScreen('')}}>AC</button>
-          <button onClick = {() => { setScreen(str => str + '/')}}>/</button>
+          <button onClick = {() => { setScreen(str => str + ' / ')}}>/</button>
           
           <button onClick = {() => { setScreen(str => str + '(')}}>(</button>
           <button onClick = {() => { setScreen(str => str + ')')}}>)</button>
@@ -49,6 +50,7 @@ function App() {
           <button style={{backgroundColor : 'green'}} onClick = {() => {
             try {
               let newVal = eval(screen.replace('x', '*').replace('(', '*('))
+              // edge case what if *( -> **(
               setScreen(() => newVal)
               setError('')
             } catch (error) {
@@ -62,10 +64,12 @@ function App() {
 
         </div>
       </div>
-      <p className="read-the-docs">
-      </p>
     </>
   )
 }
+// to do
+// comments
+
+
 
 export default App
